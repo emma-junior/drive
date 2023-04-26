4212d2f06165451d33c3c854a2e44aa896274001
emma-junior <emnzboy31@gmail.com>
2023-04-26 23:42:07 +0100

    implemented sliding content in hero section

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -5,10 +5,29 @@ import useAnimation from "../../animations";
 
 const Hero = ():ReactElement => {
   const { app } = useAnimation();
+  
   return (
     <section ref={app}  className={styles.hero}>
         <p className={`herotext ${styles.hero_p}`}>GREATNESS&nbsp;IS&nbsp;IN&nbsp;OUR&nbsp;BACKYARD</p>
         <div className={`drive ${styles.hero_drive}`}><p >DRIVE</p></div>
+        <div className={styles.hero_slides}>
+          <div className={styles.hero_slides_wrapper}>
+            {(() => {
+                const arr = [];
+                for (let i = 0; i < 2; i++) {
+                    arr.push(
+                        <div className="">
+                            <span>CAPITAL THAT COMES TO YOU</span>
+                            <span>MSP</span>
+                            <span>TRUE PARTNERS</span>
+                            <span>IDEA TO IPO</span>
+                        </div>
+                    );
+                }
+                return arr;
+            })()}
+          </div>
+        </div>
     </section>
   )
 }
