e448b5ccd0d71c2690b365fab39753bdaf2f7245
emma-junior <emnzboy31@gmail.com>
2023-04-28 18:03:41 +0100

    implemented respnsiveness for navbar n hero section

--- animations/features.tsx
+++ animations/features.tsx
@@ -15,20 +15,20 @@ const useAnimationTwo= () => {
 
     let ctx = gsap.context(() => {
         tl.current = gsap.timeline({
-            scrollTrigger: {trigger: ".featuresone", start: "top center"}
+            scrollTrigger: {trigger: ".featuresone", start: "top bottom"}
         })
         .from(".featuresone", {x: -200, opacity: 0},)
-        .to(".featuresone", {x: 0, opacity: 1})
+        .to(".featuresone", {clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', x: 0, opacity: 1})
         tl.current = gsap.timeline({
-            scrollTrigger: {trigger: ".featurestwo", start: "top center"}
+            scrollTrigger: {trigger: ".featurestwo", start: "top bottom"}
         })
         .from(".featurestwo", {y: 200, opacity: 0},)
         .to(".featurestwo", {y: 0, opacity: 1})
         tl.current = gsap.timeline({
-            scrollTrigger: {trigger: ".featuresthree", start: "top center"}
+            scrollTrigger: {trigger: ".featuresthree", start: "top bottom"}
         })
         .from(".featuresthree", {x: 200, opacity: 0},)
-        .to(".featuresthree", {x: 0, opacity: 1})
+        .to(".featuresthree", {clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', x: 0, opacity: 1})
         .to(".topic", { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' },)
         .to(".char", { y: 0, stagger: 0.05, opacity: 1, delay:0.2, duration: .1 },)
         .from(".info", {y:100, opacity: 0},)
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {ReactElement, useState} from 'react'
 import Image from 'next/image'
 import styles from "./navbar.module.scss"
 import useAnimation from '../../animations';
@@ -6,15 +6,22 @@ import useAnimation from '../../animations';
 
 const Navbar = () => {
   const { app } = useAnimation();
+  const [click, setClick] = useState<boolean>(false);
+  const handleClick = () => setClick(!click)
   return (
     <nav ref={app} className={styles.navbar}>
         <Image className={` logo ${styles.navbar_logo}`} src="/svgs/logo.svg" alt='' width={150} height={60} />
-        <ul>
-          <li className='menuitem'>STORY</li>
-          <li className='menuitem'>TEAM</li>
-          <li className='menuitem'>PORTFOLIO</li>
-          <li className='menuitem'>TALENT</li>
-        </ul>
+        <div className={click ? styles.showSidebar : styles.navLinks}>
+          <ul>
+            <li className='menuitem'>STORY</li>
+            <li className='menuitem'>TEAM</li>
+            <li className='menuitem'>PORTFOLIO</li>
+            <li className='menuitem'>TALENT</li>
+          </ul>
+        </div>
+        <div className={`menuitem ${styles.mobileIcon}`} onClick={handleClick}>
+                {!click ?  <Image src="/svgs/bar.svg" alt='' width={40} height={40} />: <Image src="/svgs/times.svg" alt='' width={40} height={40} />}
+        </div>
     </nav>
   )
 }
