46d7f770e8c2788831d7bfac21842e5035db1087
Emmanuel <emnzboy31@gmail.com>
2025-04-09 15:05:34 +0100

    adjusted the scrub on the animation for portfolio section

--- animations/portfolio.tsx
+++ animations/portfolio.tsx
@@ -1,36 +1,39 @@
 import React, { useEffect, useRef } from "react";
-import { gsap } from 'gsap'
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const usePortfolioAnimation = () => {
+  let app = useRef<HTMLDivElement>(null);
+  const tl = useRef<GSAPTimeline>();
 
-    let app = useRef<HTMLDivElement>(null);
-    const tl = useRef<GSAPTimeline>()
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      ScrollTrigger.saveStyles(".first, .second");
 
-     useEffect(() => {
+      tl.current = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: ".marquee",
+            start: "bottom bottom",
+            scrub: 1,
+          },
+        })
+        .to(".first", { duration: 2, x: -300 })
+        .to(".second", { duration: 2, x: 300 }, "<");
 
-        let ctx = gsap.context(() => {
-            ScrollTrigger.saveStyles(".first, .second");
+      tl.current = gsap
+        .timeline({
+          scrollTrigger: { trigger: ".content", start: "top bottom" },
+        })
+        .from(".content", { y: 100, opacity: 0 })
+        .to(".content", { y: 0, opacity: 1 });
+    }, app);
 
-            tl.current = gsap.timeline({
-                scrollTrigger: {trigger: ".marquee", start: "bottom bottom", scrub: 7}
-            })
-            .to(".first", {duration: 2, x: -300})
-            .to(".second", {duration: 2, x: 300},"<")
+    return () => ctx.revert();
+  }, []);
 
-            tl.current = gsap.timeline({
-                scrollTrigger: {trigger: ".content", start: "top bottom"}
-            })
-            .from(".content", {y:100, opacity: 0},)
-            .to(".content", {y:0, opacity: 1},)
+  return { app };
+};
 
-        }, app);
-
-    return () => ctx.revert()
-  }, [])
-    
-    return {app}
-}
-
-export default usePortfolioAnimation
+export default usePortfolioAnimation;
